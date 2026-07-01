1416750d4d7796b6e2e48ede6775222f740d403c
radhikagupta25 <radhikaaaguptaaa25@gmail.com>
2026-07-01 19:10:17 +0530

    Setup user controller and route

--- Orbit-Backend/src/utils/ApiError.js
+++ Orbit-Backend/src/utils/ApiError.js
@@ -6,11 +6,11 @@ class ApiError extends Error {
         stack = "",
     ) {
         super(message)
-        this.statusCode = statusCode,
-            this.data = null
-        this.message = message,
-            this.success = false
-        this.errors = this.errors
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.success = false
+        this.errors = errors
 
         if (stack) {
             this.stack = stack;
@@ -20,4 +20,4 @@ class ApiError extends Error {
     }
 }
 
-export {ApiError}
+export { ApiError }
